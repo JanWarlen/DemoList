312fdb088d4e50e07af26a026c66d885d485ad78
wangjian <wangjian@shulie.io>
2018-12-27 15:13:03 +0800

    1. 避免No runnable methods错误

--- MockitoPowerMockDemo/src/test/java/com/janwarlen/BasedTest.java
+++ MockitoPowerMockDemo/src/test/java/com/janwarlen/BasedTest.java
@@ -1,5 +1,6 @@
 package com.janwarlen;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -12,4 +13,9 @@ import org.powermock.modules.junit4.PowerMockRunner;
 @RunWith(PowerMockRunner.class)
 @PowerMockIgnore({"javax.net.ssl.*","javax.crypto.*","org.xml.*", "javax.xml.*"})
 public class BasedTest {
+
+    @Test
+    public void baseTest() {
+        System.out.println("hello mockito.");
+    }
 }
