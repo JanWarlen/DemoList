264f8bbad2b0f19cbc674db24c8836787158b8d3
wangjian <wangjian@shulie.io>
2019-01-22 15:02:44 +0800

    1. 简单的socket demo

---
+++ BasicGrammarDemo/src/test/java/com/janwarlen/socketdemo/SocketClientTest.java
@@ -0,0 +1,18 @@
+package com.janwarlen.socketdemo;
+
+import org.junit.Test;
+
+/**
+ * @ClassName: SocketClientTest
+ * @author: janwarlen
+ * @Date: 2019/1/21 17:46
+ * @Description:
+ */
+public class SocketClientTest {
+
+    public static void main(String[] args) {
+        // bye 退出
+        SocketClient socketClient = new SocketClient("127.0.0.1", 1999);
+        socketClient.start();
+    }
+}
---
+++ BasicGrammarDemo/src/test/java/com/janwarlen/socketdemo/SocketServerTest.java
@@ -0,0 +1,18 @@
+package com.janwarlen.socketdemo;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author: janwarlen
+ * @Date: 2019/1/21 17:44
+ * @Description:
+ */
+public class SocketServerTest {
+
+    public static void main(String[] args) {
+        // q 退出
+        SocketServer socketServer = new SocketServer(1999);
+        socketServer.start();
+    }
+}
