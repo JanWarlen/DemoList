737f4fb5c3a7ed1b6166841b79d9117aa8cec07f
wangjian <wangjian@shulie.io>
2019-09-11 18:59:34 +0800

    1. 去除无用import

--- SpringBootDemoList/SpringBootDubboDemo/src/main/java/com/janwarlen/dubbo/DubboReference.java
+++ SpringBootDemoList/SpringBootDubboDemo/src/main/java/com/janwarlen/dubbo/DubboReference.java
@@ -1,14 +1,6 @@
 package com.janwarlen.dubbo;
 
-import com.alibaba.dubbo.config.annotation.Reference;
-import com.deppon.data.platform.common.service.ResultDTO;
-import com.deppon.data.platform.demo.api.facade.dto.DemoDTO;
-import com.deppon.data.platform.demo.api.facade.service.DemoDubboServiceFacade;
-import io.shulie.center.user.api.model.entity.Org;
-import io.shulie.center.user.api.service.OrgService;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 /**
  * @ClassName: DubboReference
