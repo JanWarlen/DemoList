38d6bb93e52452702b11d0addc4971bf1b26de6a
wangjian <wangjian@shulie.io>
2019-09-11 19:01:48 +0800

    1. 添加SpringBoot2.x版本demo集合 2. 添加redis多模式兼容demo

---
+++ SpringBootDemoList2/SpringBootRedisModeDemo/src/main/java/com/janwarlen/SpringBootRedisModeDemoApplication.java
@@ -0,0 +1,17 @@
+package com.janwarlen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: SpringBootRedisModeDemoApplication
+ * @author: wangjian
+ * @Date: 2019/9/11 18:13
+ * @Description:
+ */
+@SpringBootApplication
+public class SpringBootRedisModeDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisModeDemoApplication.class, args);
+    }
+}
