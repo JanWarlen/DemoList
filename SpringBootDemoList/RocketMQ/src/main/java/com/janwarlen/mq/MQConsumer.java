f6889f708b2b4b84c2f8d35306835942db175a7e
wangjian <wangjian@shulie.io>
2019-10-31 14:58:45 +0800

    1. re group

--- SpringBootDemoList/RocketMQ/src/main/java/com/janwarlen/mq/MQConsumer.java
+++ SpringBootDemoList/RocketMQ/src/main/java/com/janwarlen/mq/MQConsumer.java
@@ -1,11 +1,14 @@
 package com.janwarlen.mq;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +21,9 @@ import javax.annotation.PostConstruct;
  */
 @Component
 public class MQConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MQConsumer.class);
+
     /**
      * 消费者的组名
      */
@@ -29,6 +35,11 @@ public class MQConsumer {
      */
     @Value("${apache.rocketmq.namesrvAddr}")
     private String namesrvAddr;
+
+    @Value("${rocketmq.consumer.topic}")
+    private String topic;
+    @Value("${rocketmq.consumer.tag}")
+    private String tag;
 
     /**
      * 消费者
@@ -45,7 +56,7 @@ public class MQConsumer {
         consumer.setVipChannelEnabled(false);
         try {
             //订阅PushTopic下Tag为push的消息
-            consumer.subscribe("TopicTest", "push");
+            consumer.subscribe(topic, tag);
 
             //设置Consumer第一次启动是从队列头部开始消费还是队列尾部开始消费
             //如果非第一次启动，那么按照上次消费的位置继续消费
@@ -54,11 +65,18 @@ public class MQConsumer {
                 try {
                     for (MessageExt messageExt : list) {
 
-                        System.out.println("messageExt: " + messageExt);//输出消息内容
+                        String tags = messageExt.getTags();
+                        if (tags.contains("heart")) {
+                            continue;
+                        }
+
+                        LOGGER.info("messageExt properties: " + JSONObject.toJSONString(messageExt.getProperties()));
+                        //输出消息内容
 
                         String messageBody = new String(messageExt.getBody(), RemotingHelper.DEFAULT_CHARSET);
 
-                        System.out.println("消费响应：msgId : " + messageExt.getMsgId() + ",  msgBody : " + messageBody);//输出消息内容
+//                        LOGGER.info("消费响应：msgId : " + messageExt.getMsgId() + ",  msgBody : " + messageBody);
+                        //输出消息内容
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
